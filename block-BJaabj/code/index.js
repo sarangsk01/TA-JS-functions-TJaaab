66424056b417d34fa7c308268e110970d5e9bff5
sarangsk01 <95225059+sarangsk01@users.noreply.github.com>
2022-02-22 17:56:44 +0530

    Update index.js

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -1,7 +1,11 @@
 /*
 1. Create a function named `sayHello` that alerts a message `Hello World!`.
 */
-
+function sayHello (){
+alert(`Hello World!`);
+}
+//function call
+ sayHello ();
 /*
 2. Create a function named `fullName` with the following steps:
 
@@ -10,7 +14,13 @@
   - Concat the value of fistName and lastName and store it in another variable named `fullName`.
   - Alert `fullName`
 */
-
+function fullName (){
+  let firstName = prompt(`Enter your first name!`);
+  let lastName = prompt(`Enter your last name!`);
+  let fullName = (firstName + " " + lastName);
+  alert (fullName);
+}
+fullName ();
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
 
@@ -19,7 +29,13 @@
   - Add the value of firstNum and secondNum and store it in another variable named `sum`.
   - Alert `sum`
 */
-
+function addTwoNumbers(){
+  let firstNum = +prompt(`Enter the first number!`);
+  let secondNum = +prompt(`Enter the second number!`);
+  let sum = firstNum + secondNum;
+  alert(sum);
+}
+addTwoNumbers()
 /*
 4. Create a function named `getTable` with the following steps:
 
@@ -38,14 +54,20 @@
   9 * 9 = 81
   9 * 10 = 90
 */
-
+function getTable(){
+  let num = +prompt(`Enter a number!`);
+}
+getTable ();
 /*
 5. Create a function named `isLeapYear` with the following steps:
 
   - Prompts a message saying `Enter a year` and store the value into variable named`year`
   - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
-
+function isLeapYear(){
+  let year = +prompt(`Enter a year!`);
+}
+isLeapYear ();
 /*
 6. Create a function named `getFactorial` with the following steps:
 
@@ -53,3 +75,8 @@
   - Alert `The factorial of [number] is [factorial]`
 
 */
+function getFactorial(){
+let number = +prompt(`Enter a number!`);
+alert(`The factorial of ${number} is ${ number! =number * (nu-1)! } `);
+}
+getFactorial();
